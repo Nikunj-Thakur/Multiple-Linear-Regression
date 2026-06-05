13fe2758b3387ffd93cd712ba2d409f15cf18b83
Nikunj Thakur <nik.thak22@gmail.com>
2026-06-05 16:43:45 +0100

    update routines

--- house_price_prediction_model.py
+++ house_price_prediction_model.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import model_parameters as mp
 
@@ -21,3 +22,20 @@ print(cost)
 
 dj_dw,dj_db=mp.compute_gradient(X_train,y_train,w_init,b_init)
 print(f"Dj_Dw={dj_dw} and Dj_Db={dj_db}")
+
+# initialize parameters
+initial_w = np.zeros_like(w_init)
+initial_b = 0.
+alpha=5.0e-7
+w_final,b_final,J_hist=mp.gradient_descent(X_train,y_train,initial_w,initial_b,alpha,1000)
+
+print(w_final, b_final)
+
+# plot cost versus iteration  
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
+ax1.plot(J_hist)
+ax2.plot(100 + np.arange(len(J_hist[100:])), J_hist[100:])
+ax1.set_title("Cost vs. iteration");  ax2.set_title("Cost vs. iteration (tail)")
+ax1.set_ylabel('Cost')             ;  ax2.set_ylabel('Cost') 
+ax1.set_xlabel('iteration step')   ;  ax2.set_xlabel('iteration step') 
+plt.show()
--- model_parameters.py
+++ model_parameters.py
@@ -1,4 +1,5 @@
 import math
+import copy
 import numpy as np
 
 
@@ -24,14 +25,34 @@ def compute_cost(X, y, w, b):
 
 
 def compute_gradient(X, y, w, b):
-    m = X.shape[0]
-    n = X.shape[1]
-    dj_dw = np.zeros((n,))
-    dj_db = 0.
+    m = X.shape[0]  # i
+    n = X.shape[1]  # j
+    dj_dw = np.zeros((n,))  # 1D array/vector
+    dj_db = 0.0
 
-    for j in range(n):
-        for i in range(m):
-            f_wb = np.dot(X[i], w) + b
-            dj_dw[j] = dj_dw[j] + (f_wb-y[i])*X[i,j]
+    for i in range(m):  # i ranges from 0,1,2
+        f_wb = np.dot(X[i], w) + b
         dj_db = dj_db + (f_wb-y[i])
+
+        for j in range(n):  # j=0,1,2,3
+            dj_dw[j] = dj_dw[j] + (f_wb-y[i])*X[i, j]
     return dj_dw/m, dj_db/m
+
+
+def gradient_descent(X, y, w_init, b_init, alpha, no_of_iterations):
+    J_history = []
+    w = copy.deepcopy(w_init)
+    b = b_init
+
+    for i in range(no_of_iterations):
+        dj_dw, dj_db = compute_gradient(X, y, w, b)
+        w = w-alpha*dj_dw
+        b = b-alpha*dj_db
+
+        if (i < 100000):
+            J_history.append(compute_cost(X, y, w, b))
+
+        if i % math.ceil(no_of_iterations/10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}")
+
+    return w, b, J_history
