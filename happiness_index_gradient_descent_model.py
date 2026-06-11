e28258da71de3e0f777ffce77ea68eb9b70a655f
Nikunj Thakur <nik.thak22@gmail.com>
2026-06-11 21:28:15 +0100

    add parameter standardization

--- happiness_index_gradient_descent_model.py
+++ happiness_index_gradient_descent_model.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import model_parameters as mp
+import utility_functions as uf
 import os
 
 os.chdir(os.path.dirname(__file__))
 df = pd.read_csv("WHR_2024.csv")
 df = df.dropna()  # dropping small percentage of rows have missing values
 X_train = df.iloc[:, 3:9].values  # Features (columns 3-8)
+
+# Feature Standardization (Z-score normalization)
+# computes the mean of each column (feature) in X_train.
+X_mean = X_train.mean(axis=0)
+# calculates the standard deviation of each feature (column) in X_train
+X_std = X_train.std(axis=0)
+X_train = (X_train - X_mean) / X_std
+
 y_train = df['happiness_score'].values  # Target variable (column 2)
 
 X_features = ['gdp_per_capita', 'social_support', 'healthy_life_expectancy', 'freedom_to_make_life_choices',
@@ -25,10 +33,10 @@ plt.show()
 initial_w = np.zeros((6,))
 initial_b = 0.
 alpha = 0.01
-w_final, b_final, J_hist = mp.gradient_descent(
+w_final, b_final, J_hist = uf.gradient_descent(
     X_train, y_train, initial_w, initial_b, alpha, 2000)
 
-print(w_final, b_final)
+print(f"Final Weights: {w_final}, Bias : {b_final}\n")
 
 # plot cost versus iteration
 fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
@@ -42,4 +50,14 @@ ax1.set_xlabel('iteration step')
 ax2.set_xlabel('iteration step')
 plt.show()
 
-print((np.dot(X_train[19],w_final)) + b_final)
+print("Predict the happiness index of country 'India' having features:")
+test_row = np.array([1.166, 0.653, 0.417, 0.767, 0.174, 0.122])
+print(f"{test_row}")
+# Scale test data using training set statistics
+test_row_scaled = (test_row - X_mean) / X_std
+print("Scaled India features:\n", test_row_scaled)
+
+prediction = (np.dot(test_row_scaled, w_final)) + b_final
+print(f"Predicted Happiness Index: {prediction:0.2f}")
+print(f"Actual Happiness Index: 4.05")
+print(f"Prediction Error: {abs(prediction - 4.05):0.2f}")
